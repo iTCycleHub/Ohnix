f31341cc09237ffe82397afac9fbcc899d0fff8a
Alejando Vallejo <alejandrosoftware.engineering@gmail.com>
2025-02-24 20:06:32 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from './app.js';
+dotenv.config({
+    path: './.env'
+})
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World !!</h1>');
+});
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`✅ Server listening on http://localhost:${process.env.PORT}/`);
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed !!! ", err);
+    })
