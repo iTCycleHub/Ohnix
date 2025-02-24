8c0abc66afe430d1dc453701f7faadd43654e119
Alejando Vallejo <alejandrosoftware.engineering@gmail.com>
2025-02-24 20:20:58 +0530

    feat: add initial setup for bcrypt and jsonwebtoken modules, including user routes

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser
+} from "../controllers/user.controller.js";
+import {upload} from "../middleware/multer.middleware.js"
+import { verifyJWT } from "../middleware/auth.middleware.js";
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([{
+        name: "avatar",
+        maxCount: 1
+    }]),
+    registerUser
+);
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+export default router;
