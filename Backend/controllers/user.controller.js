0d18c7d1a553412f9b185d8109c821d62e569a4a
Alejando Vallejo <alejandrosoftware.engineering@gmail.com>
2025-02-24 20:21:41 +0530

    refactor: remove unnecessary blank line in changeCurrentPassword function

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -233,7 +233,6 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, "Password changed successfully"))
 });
 
-
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res
         .status(200)
