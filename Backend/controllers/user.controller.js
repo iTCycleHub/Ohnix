bfc52f6004c85e84565c78971aed7910c7701375
Alejando Vallejo <alejandrosoftware.engineering@gmail.com>
2025-03-03 01:31:44 +0530

    fix: remove TODO comment for deleting old avatar image in updateUserAvatar function

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -312,9 +312,7 @@ const updateUserAvatar = asyncHandler(async (req, res, next) => {
     if (!avatarLocalPath) {
         return next(new ApiError(400, "Avatar file is missing"));
     }
-
-    //TODO: delete old image - assignment
-
+    
     const avatar = await uploadOnCloudinary(avatarLocalPath);
 
     if (!avatar.url) {
