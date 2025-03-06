b328014e8becda3939f71ab0baae224c5c6c993d
Alejando Vallejo <alejandrosoftware.engineering@gmail.com>
2025-03-06 14:17:29 +0530

    feat: add aggregation method to retrieve products with category and unit details

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -97,4 +97,52 @@ productSchema.statics.updateStock = async function (
     }
 };
 
+productSchema.statics.getProductsWithDetails = async function () {
+    try {
+        const products = await this.aggregate([
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "category_id",
+                    foreignField: "_id",
+                    as: "category",
+                },
+            },
+            {
+                $lookup: {
+                    from: "units",
+                    localField: "unit_id",
+                    foreignField: "_id",
+                    as: "unit",
+                },
+            },
+            {
+                $unwind: "$category",
+            },
+            {
+                $unwind: "$unit",
+            },
+            {
+                $project: {
+                    _id: 1,
+                    product_name: 1,
+                    product_code: 1,
+                    buying_price: 1,
+                    selling_price: 1,
+                    stock: 1,
+                    product_image: 1,
+                    category_name: "$category.category_name",
+                    unit_name: "$unit.unit_name",
+                    createdAt: 1,
+                    updatedAt: 1,
+                },
+            },
+        ]);
+
+        return products;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
 export const Product = mongoose.model("Product", productSchema);
