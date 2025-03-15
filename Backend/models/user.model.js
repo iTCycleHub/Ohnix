79947ab0ba4b7ce936f7338c4079e866820c74f0
Alejando Vallejo <alejandrosoftware.engineering@gmail.com>
2025-03-15 11:29:08 +0530

    fix: correct typo in user model schema for lowercase property

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema(
             type: String,
             required: true,
             unique: true,
-            lowecase: true,
+            lowercase: true,
             trim: true,
         },
         password: {
